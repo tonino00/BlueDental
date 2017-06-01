5acfc26a2e42ee186a14ff184bf587b25266f92b
Antonino C.Gama <antoninocgama@hotmail.com>
2017-06-01 11:02:24 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+var campos = [
+	document.querySelector('#name'),
+	document.querySelector('#clinic'),
+	document.querySelector('#turn'),
+	document.querySelector('#date')
+];
+
+	console.log(campos);
+
+var tbody = document.querySelector('table tbody');
+
+
+document.querySelector('.form').addEventListener('submit', function(event){
+	event.preventDefault();
+
+	var tr = document.createElement('tr');
+	campos.forEach(function(campo){
+		var td = document.createElement('td');
+		td.textContent = campo.value;
+		tr.appendChild(td);
+	});
+
+	tbody.appendChild(tr);
+
+	campos[0].value = '';
+	campos[1].value = '';
+	campos[2].value = '';
+	campos[3].value = '';
+
+	campos[0].focus();
+});
